f59589da7ea612cbb3c31390ed18b79197af73bf
lzkelley <lkelley@cfa.harvard.edu>
2016-06-22 10:51:16 -0400

    ENH: moved some old constants into storage file.

---
+++ NOTES.py
@@ -0,0 +1,59 @@
+
+_ILLUSTRIS_DETAILS_FILENAME_REGEX = "blackhole_details_*.txt"
+
+_ILLUSTRIS_MERGERS_FILENAME_REGEX = "blackhole_mergers_*.txt"
+
+_ILLUSTRIS_DETAILS_DIRS = {3: "/n/ghernquist/Illustris/Runs/L75n455FP/"
+                              "output/blackhole_details/",
+
+                           2: "/n/ghernquist/Illustris/Runs/L75n910FP/"
+                              "combined_output/blackhole_details/",
+
+                           1: ["/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-curie/blackhole_details/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-supermuc/blackhole_details/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Aug8/blackhole_details/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Aug14/blackhole_details/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Oct10/blackhole_details/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Sep25/blackhole_details/"]
+                           }
+
+_ILLUSTRIS_MERGERS_DIRS = {3: "/n/ghernquist/Illustris/Runs/L75n455FP/"
+                              "output/blackhole_mergers/",
+
+                           2: "/n/ghernquist/Illustris/Runs/L75n910FP/"
+                              "combined_output/blackhole_mergers/",
+
+                           1: ["/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-curie/blackhole_mergers/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-supermuc/blackhole_mergers/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Aug8/blackhole_mergers/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Aug14/blackhole_mergers/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Sep25/blackhole_mergers/",
+
+                               "/n/ghernquist/Illustris/Runs/L75n1820FP/"
+                               "txt-files/txtfiles_new/txt-files-partial/Oct10/blackhole_mergers/"]
+                           }
+
+_ILLUSTRIS_SUBBOX_TIMES_FILENAMES = \
+    "/n/ghernquist/Illustris/Runs/{:s}/postprocessing/subboxes_times.txt"
+
+_SNAPSHOT_COSMOLOGY_DATA_FILENAME = "illustris-snapshot-cosmology-data.npz"
